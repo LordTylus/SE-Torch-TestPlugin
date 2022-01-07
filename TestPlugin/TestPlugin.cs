051638afef7276d97d739be1d6c83f7afe64f1fc
LordTylus <martin.roekker@gmail.com>
2022-01-07 23:46:04 +0100

    Plugin page added

--- TestPlugin/TestPlugin.cs
+++ TestPlugin/TestPlugin.cs
@@ -1,6 +1,12 @@
 ﻿using NLog;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.World;
 using Sandbox.ModAPI;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading;
 using System.Windows.Controls;
@@ -10,7 +16,11 @@ using Torch.API.Managers;
 using Torch.API.Plugins;
 using Torch.API.Session;
 using Torch.Managers.PatchManager;
+using Torch.Mod;
 using Torch.Session;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.ObjectBuilders;
 
 namespace TestPlugin {
 
@@ -38,9 +48,112 @@ namespace TestPlugin {
             Save();
         }
 
-        private void SessionChanged(ITorchSession session, TorchSessionState newState) {
+        private void SessionChanged(ITorchSession session, TorchSessionState state) {
 
-            Log.Info("Session-State is now " + newState);
+            switch (state) {
+
+                case TorchSessionState.Loaded:
+
+                    var multiplayerManagerBase = Torch.CurrentSession.Managers.GetManager<IMultiplayerManagerBase>();
+
+                    if (multiplayerManagerBase != null) {
+
+                        multiplayerManagerBase.PlayerJoined += PlayerJoined;
+                        multiplayerManagerBase.PlayerLeft += PlayerLeft;
+
+                    } else {
+                        Log.Warn("No multiplayer manager loaded!");
+                    }
+
+                      break;
+
+                case TorchSessionState.Unloading:
+
+                    if (multiplayerManagerBase != null) {
+
+                        multiplayerManagerBase.PlayerJoined -= PlayerJoined;
+                        multiplayerManagerBase.PlayerLeft -= PlayerLeft;
+                    }
+
+                    break;
+            }
+        }
+
+        public void DoStuff(MySlimBlock slimBlock, ReadOnlyDictionary<MyDefinitionId, int> amountDict) {
+
+            MyCubeGrid grid = null;
+            if(grid.PlayerPresenceTier == MyUpdateTiersPlayerPresence.Normal) {
+                //DO YOUR STUFF
+            }
+
+
+            MyCubeBlock block = slimBlock.FatBlock;
+
+            /* Copy the dictiornary as I want to modify it */
+            var typeIdDict = new Dictionary<MyDefinitionId, int>(amountDict);
+
+            /* all Inventories */
+            for (int i = 0; i < block.InventoryCount; i++) {
+                
+                var inventory = block.GetInventory(i);
+
+                var itemsList = inventory.GetItems();
+
+                /* We loop through the items in reverse otherwise the we run out of bounds. */
+                for(int j = itemsList.Count; j >= 0; j--) {
+
+                    var item = itemsList[j];
+                    var definitionId = item.Content.GetObjectId();
+
+                    /* If that definition is not in dictionary ignore. */
+                    if(typeIdDict.TryGetValue(definitionId, out int value)) {
+
+                        value--;
+
+                        /* checking if we are below defined limit */
+                        if (value < 0)
+                            inventory.RemoveItemsAt(j);
+
+                        /* writing reduced value back in dictionary */
+                        typeIdDict[definitionId] = value;
+                    }
+                }
+
+                /* We probably (most likely) only need to refresh it once. after we are done fiddling around with it. */
+                inventory.Refresh();
+            }
+        }
+
+        private void PlayerLeft(IPlayer player) {
+
+            long idendity = MySession.Static.Players.TryGetIdentityId(player.SteamId);
+
+            Log.Debug("Player " + player.Name + " with ID " + player.SteamId + " and Identity " + idendity + " left.");
+
+            if (idendity == 0)
+                return;
+
+            if (Config.RemoveGpsOnJoin) {
+
+                Log.Debug("Removing Biggest Grid GPS for Player #" + idendity);
+                RemoveGpsFromPlayer(idendity);
+            }
+        }
+
+        private void PlayerJoined(IPlayer player) {
+
+            long idendity = MySession.Static.Players.TryGetIdentityId(player.SteamId);
+
+            Log.Debug("Player " + player.Name + " with ID " + player.SteamId + " and Identity " + idendity + " joined.");
+
+            if (idendity == 0)
+                return;
+
+            if (Config.RemoveGpsOnJoin) {
+
+                Log.Debug("Removing Biggest Grid GPS for Player #" + idendity);
+                RemoveGpsFromPlayer(idendity);
+            }
         }
 
         private void SetupConfig() {
@@ -73,4 +186,15 @@ namespace TestPlugin {
             }
         }
     }
+}
+[Command("whatsmyip")]
+[Permission(MyPromoteLevel.None)]
+public void GetIP(ulong steamId = 0) {
+    if (steamId == 0)
+        steamId = Context.Player.SteamUserId;
+
+    VRage.GameServices.MyP2PSessionState statehack = new VRage.GameServices.MyP2PSessionState();
+    MySteamServiceWrapper.Static.Peer2Peer.GetSessionState(steamId, ref statehack);
+    var ip = new IPAddress(BitConverter.GetBytes(statehack.RemoteIP).Reverse().ToArray());
+    Context.Respond($"Your IP is {ip}");
 }
